0b7899145daf1ceda531990c83e5b8c7a5c1d5eb
M511-49\admin <s3a432053@student.ncut.edu.tw>
2017-10-16 08:46:26 +0800

    選擇性路由參數

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,6 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('student/{student_no/score/(subject?)}', function ($student_no,$subhect = null) {
+    return "學號：".$student_no."的".((is_null($subhect))?"所有科目":$subhect)."成績";
 });
