675551accdba7d6ce6c7a078fa80e8ce7f5e90cc
M511-49\admin <s3a432053@student.ncut.edu.tw>
2017-10-16 09:41:35 +0800

    正規表達式限制參數

--- routes/web.php
+++ routes/web.php
@@ -11,11 +11,11 @@
 |
 */
 
-
+/*
 Route::get('student/{student_no}/score/{subject?}', function ($student_no,$subject = null) {
     return "學號：".$student_no."的".((is_null($subject))?"所有科目":$subject)."成績";
 });
-
+*/
 Route::get('student/{student_no}', function ($student_no) {
     return "學號：".$student_no;
 })->where(['student_no'=>'s[0-9]{10}']);
