ac490567ca27c9c40c9b8ea177a43be2f7a99d99
a0952288480 <s3a432053@student.ncut.edu.tw>
2017-10-20 20:33:11 +0800

    修改nav.blade.php視圖上的超連接，使之可超連接至board視圖

--- resources/views/partials/nav.blade.php
+++ resources/views/partials/nav.blade.php
@@ -1,9 +1,9 @@
 <nav class="navbar navbar-default navbar-fixed-top">
     <div class="container">
-        <a class="navbar-brand" href="/">HelloLaravel</a>
+        <a class="navbar-brand" href="http://localhost:8000/">HelloLaravel</a>
         <ul class="nav navbar-nav">
             <li>
-                <a href="#">排行榜</a>
+                <a href="{{action('BoardController@getIndex')}}">排行榜</a>
             </li>
         </ul>
     </div>
