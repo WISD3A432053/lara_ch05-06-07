c2da11950f0930da25c39188c91a60f80c254285
3A432053許漢昌 <s3a432053@student.ncut.edu.tw>
2017-11-26 15:13:20 +0800

    正確套用master視圖的佈局

--- resources/views/score.blade.php
+++ resources/views/score.blade.php
@@ -1,30 +1,7 @@
-<!DOCTYPE html>
-<html>
-<head>
-	<title>詳細資料</title>
-	<meta charset="UTF-8">
-	<link rel="stylesheet" type="text/css" href="{{ asset('css/bootstrap.min.css') }}" />
-	<script type="text/javascript" src="{{ asset('js/jquery.min.js') }} "></script>
-	<script type="text/javascript" src="{{ asset('js/bootstrap.min.js') }}"></script>
-</head>
-<body>
-	<nav class="navbar navbar-default navbar-fixed-top">
-		<div class="container">
-			<a class="navbar-brand" href="/">HelloLaravel</a>
-			<ul class="nav navbar-nav">
-				<li>
-					<a href="#">排行榜</a>
-				</li>
-			</ul>
-		</div>
-	</nav>
-	<div style="padding-top: 70px;"></div>
-	<div class="container">
-		@extends('layouts.master')
+@extends('layouts.master')
+@section('title','詳細資料')
+@section('content')
 
-		@section('title','詳細資料')
-
-		@section('content')
 		<div class="page-header">
 			<h1>詳細資料</h1>
 		</div>
@@ -55,5 +32,4 @@
 			@endif
 		</div>
 	</div>
-</body>
-</html>
+@stop
